bfa5f2343349c23a3145c40422645c4875cad802
Ayush Gupta <77526719+ayush20501@users.noreply.github.com>
2025-01-15 09:04:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
 from pytube import Search
-
 app = FastAPI()
-
 @app.get("/youtube-search")
 def youtube_search(query: str, max_results: int = 5):
     try:
         search = Search(query)
         results = []
         for video in search.results[:max_results]:
-            if video.length < 60:
-                continue
             results.append(f"https://www.youtube.com/watch?v={video.video_id}")
         return {"query": query, "results": results}
     except Exception as e:
